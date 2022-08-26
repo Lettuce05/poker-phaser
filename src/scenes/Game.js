44b1d48db1c4b21f7b289746ab810eae6c131257
Anthony Romaine <anthony.romaine1@gamil.com>
2022-08-26 05:01:07 +0000

    Setup flip animation

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,7 +6,11 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super('game');
   }
+
   displaySize;
+  FLIP_SPEED = 200;
+  FLIP_ZOOM = 1.2;
+
   init() {
     // set background color
     this.mainCamera = this.cameras.add(0, 0);
@@ -28,12 +32,65 @@ export default class Game extends Phaser.Scene {
     for (let i = 0; i < 5; i++) {
       let card = this.add.image(this.scale.width * cardPositions[i][this.displaySize], this.scale.height * 0.5, 'cards', 'S7.png').setScale(cardScale[this.displaySize]);
       card.cardId = i;
+      card.isFlipping = false;
       this.cardObjects.push(card);
     }
 
+
+    this.flipCard(this.cardObjects, 0, `HA.png`, 0);
+
+
+    // this.input.on('gameobjectdown', this.handleClick);
   }
 
   update() {
 
   }
+
+  // check if card is flipping and set to true before calling
+  flipCard(gameObject, flipNum, frame, delay) {
+    // first tween: we raise and flip the card
+    this.tweens.add({
+      targets: gameObject,
+      duration: this.FLIP_SPEED / 2,
+      delay: delay,
+      scaleX: 0,
+      scaleY: this.FLIP_ZOOM * cardScale[this.displaySize],
+      ease: 'Linear',
+      onCompleteParams: [flipNum, frame],
+      onComplete: function(tween, targets, flipNum, frame) {
+        let card = targets[0];
+        if (flipNum > 0) {
+          card.setFrame(frame);
+        } else {
+          card.setFrame('back.png');
+        }
+        this.parent.scene.backFlipTween.play();
+
+      }
+    });
+
+
+    this.backFlipTween = this.tweens.add({
+      targets: gameObject,
+      duration: this.FLIP_SPEED / 2,
+      scaleX: cardScale[this.displaySize],
+      scaleY: cardScale[this.displaySize],
+      paused: true,
+      ease: 'Linear',
+      onCompleteParams: [flipNum, frame],
+      onComplete: function(tween, targets, flipNum, frame) {
+        let card = targets[0];
+        if (flipNum > 0) {
+          card.isFlipping = false;
+        } else {
+          this.parent.scene.flipCard(card, 1, frame, 200)
+        }
+
+      }
+    })
+
+
+  }
+
 }
